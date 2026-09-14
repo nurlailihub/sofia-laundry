63627db6f32cccc8160698d13597358fa28fd203
nurlailihub <nurlailichaniago456@gmail.com>
2026-09-14 11:02:29 +0700

    fix: gunakan config() bukan env() di WhatsAppService agar bekerja di production

--- app/Services/WhatsAppService.php
+++ app/Services/WhatsAppService.php
@@ -16,7 +16,7 @@ class WhatsAppService
 
     public function __construct()
     {
-        $this->token = env('FONNTE_TOKEN', '');
+        $this->token = config('services.fonnte.token', env('FONNTE_TOKEN', ''));
     }
 
     /**
